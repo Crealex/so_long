8b83802eb8c68133416115f1a6b8cb1ec2aa16db
Crealex <Alexandre.tomasi80@gmail.com>
2024-11-28 21:42:52 +0100

    continuation de la decouverte + ajout de sprite des murs

--- srcs/experience.c
+++ srcs/experience.c
@@ -6,7 +6,7 @@
 /*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:28:19 by atomasi           #+#    #+#             */
-/*   Updated: 2024/11/28 15:40:37 by atomasi          ###   ########.fr       */
+/*   Updated: 2024/11/28 20:16:22 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ int img_move(int x, int y, t_data *data)
 	void *img;
 	char *relative_path = "./assets/pokeball.xpm";
 	int img_width;
+	int img_width2;
 	int img_height;
+	int img_height2;
+
+	mlx_put_image_to_window(data->mlx, data->window, data->img, 20, 20);
 	img = mlx_xpm_file_to_image(data->mlx, relative_path, &img_width, &img_height);
-	mlx_put_image_to_window(data->mlx, data->window, img, (x - x / 2), (y - y / 2));
+	mlx_put_image_to_window(data->mlx, data->window, img, x - (img_width / 2), y - (img_height / 2));
 }
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 09:35:49 by atomasi           #+#    #+#             */
-/*   Updated: 2024/11/28 15:25:34 by atomasi          ###   ########.fr       */
+/*   Updated: 2024/11/28 21:39:21 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,21 @@ int main()
 	//int x;
 	//int y;
 	char	*relative_path = "./assets/evoli.xpm";
+	char	*path_top_wall = "./assets/wall/topWall.xpm";
+	char	*path_bottom_wall = "./assets/wall/bottomwall.xpm";
+	char	*path_side_wall = "./assets/wall/sidewall.xpm";
 	int		img_width;
 	int		img_height;
+	int		horizontal;
+	int		vertical;
 
 	//x = 20;
 	//y = 20;
-	img_height = 475;
-	img_width = 475;
+	horizontal = 0;
+	vertical = 60;
 	data.mlx = mlx_init();
-	data.window = mlx_new_window(data.mlx, 1920, 1080, "Ma premiere fenetre");
-	data.img = mlx_xpm_file_to_image(data.mlx, relative_path, &img_width, &img_height);
-	if (!data.img)
-	{
-		ft_printf("error\n");
-		return (0);
-	}
+	data.window = mlx_new_window(data.mlx, 1000, 1000, "Ma premiere fenetre");
+	//data.img = mlx_xpm_file_to_image(data.mlx, relative_path, &img_width, &img_height);
 	/* data.img = mlx_new_image(data.mlx, 1920, 1080);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length, &data.endian);
 	while (y < 40)
@@ -53,11 +53,51 @@ int main()
 			x++;
 		}
 	} */
+	data.img = mlx_xpm_file_to_image(data.mlx, path_side_wall, &img_width, &img_height);
+	if (!data.img)
+	{
+		ft_printf("error\n");
+		return (0);
+	}
+	while (vertical < 990)
+	{
+		mlx_put_image_to_window(data.mlx, data.window, data.img, 0, vertical);
+		vertical += img_height;
+	}
+	vertical =60;
+	while (vertical < 990)
+	{
+		mlx_put_image_to_window(data.mlx, data.window, data.img, 992, vertical);
+		vertical += img_height;
+	}
+	data.img = mlx_xpm_file_to_image(data.mlx, path_top_wall, &img_width, &img_height);
+	if (!data.img)
+	{
+		ft_printf("error\n");
+		return (0);
+	}
+	while(horizontal < 1000)
+	{
+		mlx_put_image_to_window(data.mlx, data.window, data.img, horizontal, 0);
+		horizontal += img_width;
+	}
+	horizontal = 0;
+	data.img = mlx_xpm_file_to_image(data.mlx, path_bottom_wall, &img_width, &img_height);
+	if (!data.img)
+	{
+		ft_printf("error\n");
+		return (0);
+	}
+	while(horizontal < 1000)
+	{
+		mlx_put_image_to_window(data.mlx, data.window, data.img, horizontal, 992);
+		horizontal += img_width;
+	}
 
-	mlx_put_image_to_window(data.mlx, data.window, data.img, 20, 20);
+	//mlx_mouse_hide(data.mlx, data.window);
 	mlx_hook(data.window, 2, 1L<<0, key_press, &data);
 	mlx_hook(data.window, 17, 1L<<17, close_window, &data);
 	mlx_hook(data.window, 4, 1L<<2, mouse_press, &data);
-	mlx_hook(data.window, 6, 1L<<6, mouse_move, &data);
+	//mlx_hook(data.window, 6, 1L<<6, mouse_move, &data);
 	mlx_loop(data.mlx);
 }
